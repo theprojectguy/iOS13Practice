3658a5c651cdfc48ec30c4d24ff17a888a4071d2
Mille <valentin.mille@epitech.eu>
2020-03-29 09:14:56 +0200

    [ADD] ToDoList app, update .gitignore

---
+++ TodoeyiOS13/TodoeyiOS13/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoeyiOS13
+//
+//  Created by Valentin Mille on 28/03/2020.
+//  Copyright © 2020 Valentin Mille. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
